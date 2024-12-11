27e6702e025718cc3c11309750f2ab8e7921c3eb
CristopherRoman <169313807+CristopherRoman@users.noreply.github.com>
2024-12-11 01:26:59 -0300

    Correccion

---
+++ Evaluacion_3/Evaluacion_3.Modelos/Modelos/Estudiante.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Firebase.Database;
+
+FirebaseClient client = new FirebaseClient("https://registroestudiantes-49dad-default-rtdb.europe-west1.firebasedatabase.app/");
+
+namespace Evaluacion_3.Modelos.Modelos
+{
+    public class Estudiante
+    {
+        public string? NombreCompleto { get; set; }
+        public string? Email { get; set; }
+        public int? Edad { get; set; }
+        public string? Curso { get; set; }
+
+
+    }
+}
